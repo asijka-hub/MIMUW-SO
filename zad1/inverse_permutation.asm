ec7ab31e6db43e3065c08042363b00f5f368edfe
asijka-hub <apokalito00@gmail.com>
2023-03-12 11:09:15 +0100

    change to uncoditional

--- zad1/inverse_permutation.asm
+++ zad1/inverse_permutation.asm
@@ -8,30 +8,40 @@ section .text
 
 ; first argumet in rdi
 ; second argument in rsi
+; array pointer used to referencing array in rbx
 
 inverse_permutation:
+        push    rbx
+        mov     rbx, rsi
+
         cmp     rdi, 0
-        je      .wrong
+        jne    .n_not_zero
+        jmp     .wrong                         ; n is 0
+
+.n_not_zero:
         cmp     rdi, 2147483647
-        ja      .wrong
+        jbe     .n_okay
+        jmp     .wrong
+
+.n_okay:
 
 ; od tego momentu wiemy ze n jest poprawne
 
-        mov     rcx, rdi                        ; w rcx trzymamy licznik petli
-.check_number_correctness:
-                                                ; w rdi mamy n
-                                                ; w rsi mamy pointer na array
-        cmp     edi, dword [rsi]
-        jg      .wrong
-        add     rsi,    4
-        loop    .check_number_correctness
+;        mov     rcx, rdi                        ; w rcx trzymamy licznik petli
+;.check_number_correctness:
+;        cmp     edi, dword [rbx]
+;        jg      .wrong
+;        add     rsi,    4
+;        loop    .check_number_correctness
 
 ; od tego momentu wiemy ze wszystkie liczby w tablicy sa w dabrym zakresie tzn 0 .. n - 1
 .okay:
+        pop     rbx
         xor     rax, rax
         mov     rax, 1
         ret
 .wrong:
+        pop     rbx
         xor     rax, rax
         mov     rax, 0
         ret
