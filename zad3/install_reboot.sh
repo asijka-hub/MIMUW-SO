ef75b18a4813361fd2d91bdcc7900e73411d59b7
asijka-hub <apokalito00@gmail.com>
2023-05-08 21:39:45 +0200

    install reboot

---
+++ zad3/install_reboot.sh
@@ -0,0 +1,18 @@
+cd /usr/src/minix/servers/pm
+make
+make install
+cd ../../../lib/libc
+make 
+make install
+cd /usr/src/minix/fs/procfs
+make 
+make install
+cd /usr/src/minix/drivers/storage/ramdisk
+make 
+make install
+cd /usr/src/minix/drivers/storage/memory
+make 
+make install
+cd /usr/src/releasetools
+make do-hdboot
+/sbin/reboot	
